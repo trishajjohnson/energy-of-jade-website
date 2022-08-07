953dbc6102fded2a9cab1ca75bd626fdd54e4900
Trish <trishajjohnson@gmail.com>
2022-08-06 18:55:22 -0700

    use styled components for mui v5

--- components/Layout.js
+++ components/Layout.js
@@ -1,32 +1,30 @@
 import { Fragment } from "react";
 import Image from 'next/image';
 import Head from 'next/head';
+import CssBaseline from '@mui/material/CssBaseline';
+import { styled } from '@mui/material/styles';
 
 import Navbar from "./Navbar";
 
-import { makeStyles } from "@material-ui/core";
-
-const useStyles = makeStyles({
-    footer: {
-        backgroundColor: 'black',
-        color: 'white',
-        height: 200,
-        textAlign: 'center',
-    },
-    logo: {
-        backgroundColor: 'white',
-    },
-    footerContent: {
-        padding: 50,
-    }
+const Footer = styled('footer')({
+    backgroundColor: 'black',
+    color: 'white',
+    height: 200,
+    textAlign: 'center',
 });
 
-function Layout(props) {
-    const styles = useStyles();
-    
+const Logo = styled('span')({
+    backgroundColor: 'white'
+});
+
+const FooterContent = styled('div')({
+    padding: 50,
+});
+
+function Layout(props) {   
     return (
         <Fragment>
-
+            <CssBaseline />
             <Head>
                 <title>Energy of Jade</title>
                 <meta name="description" content="Wellness spa website." />
@@ -39,21 +37,20 @@ function Layout(props) {
                 {props.children}
             </main>
 
-            <footer className={styles.footer}>
-                <div className={styles.footerContent}>
-                    <a
+            <Footer>
+                <FooterContent>
+                <a
                     href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
                     target="_blank"
                     rel="noopener noreferrer"
                     >
                     Powered by{' '}
-                    <span className={styles.logo}>
+                    <Logo>
                         <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                    </span>
+                    </Logo>
                     </a>
-                </div>
-            </footer>
-
+                </FooterContent>
+            </Footer>
         </Fragment>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,34 @@
 import React from 'react'
 
-import { makeStyles } from '@material-ui/core/styles'
 import Header from '../components/Header';
-
 
 const packages = ['the Cleopatra', 'the Rumi', 'the Storm', 'the Right of Path', 'the Tower', 'the Phoenix', 'the Muse']
 
-const useStyles = makeStyles({
-  button: {
-     position: 'absolute',
-     top: '80%',
-     left: '50%',
-     color: '#EEC373',
-  //    maxWidth: 500, 
-  //    maxHeight: 250,
-  //    margin: 10,
-     transform: 'translate(-50%, -50%)',
-     marginLeft: 'auto',
-     marginRight: 'auto',
-     border: '1px solid #EEC373',
-     '&:hover': {
-      //    backgroundColor: '#eec37360',
-         color: 'white',
-         border: '1px solid white'
-     }
+// const useStyles = makeStyles({
+//   button: {
+//      position: 'absolute',
+//      top: '80%',
+//      left: '50%',
+//      color: '#EEC373',
+//   //    maxWidth: 500, 
+//   //    maxHeight: 250,
+//   //    margin: 10,
+//      transform: 'translate(-50%, -50%)',
+//      marginLeft: 'auto',
+//      marginRight: 'auto',
+//      border: '1px solid #EEC373',
+//      '&:hover': {
+//       //    backgroundColor: '#eec37360',
+//          color: 'white',
+//          border: '1px solid white'
+//      }
      
-  },
-});
+//   },
+// });
 
 export default function Home() {
-  
-  const styles = useStyles();
-
   return (
-    <div className={styles.container}>
+    <div>
       <Header />
     </div>
   );
